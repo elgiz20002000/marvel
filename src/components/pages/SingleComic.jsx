cf4c5a3b63885a169ffe055f111f5913045d26d1
Elgiz <elgiz2000@icloud.com>
2022-08-26 13:48:35 +0400

    corrections

--- src/components/pages/SingleComic.jsx
+++ src/components/pages/SingleComic.jsx
@@ -67,7 +67,7 @@ const View = ({comics , onComicsSelected}) => {
                 <p className="single-comic__descr">Language: {language}</p>
                 <div className="single-comic__price">{price}</div>
             </div>
-            <Link  to={'/'} className="single-comic__back">Back to all</Link>
+            <Link  to={'/comics'} className="single-comic__back">Back to all</Link>
         </div>
 
    
